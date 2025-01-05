330b6faf7dc91d922e4279deee70fe3c0f4bf39c
Menaka S <menaka442@gmail.com>
2025-01-05 23:38:25 +0530

    add Course relationship in Student entity

--- src/main/java/com/zealtrack/crud_demo/entity/Student.java
+++ src/main/java/com/zealtrack/crud_demo/entity/Student.java
@@ -2,6 +2,9 @@ package com.zealtrack.crud_demo.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "student")
 public class Student {
@@ -19,6 +22,16 @@ public class Student {
 
     @Column(name = "email")
     private String email;
+
+    @ManyToMany(
+            fetch = FetchType.LAZY,
+            cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(
+            name = "course_student",
+            joinColumns = @JoinColumn(name = "student_id"),
+            inverseJoinColumns = @JoinColumn(name = "course_id")
+    )
+    private List<Course> courses;
 
     public Student() {
 
@@ -60,6 +73,23 @@ public class Student {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    // add a convenience method
+    public void addCourse(Course theCourse) {
+        if (courses == null) {
+            courses = new ArrayList<>();
+        }
+
+        courses.add(theCourse);
     }
 
     @Override
